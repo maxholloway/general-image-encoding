a368e15bc640adcd0dc37d61c930903832c6f943
Max Holloway <maxwellpearmanholloway@gmail.com>
2019-08-20 17:33:08 -0500

    removed; should never have been here

--- mlpythonenv/lib/python3.6/site-packages/featuretools/demo/api.py
+++
@@ -1,4 +0,0 @@
-# flake8: noqa
-from .flight import load_flight
-from .mock_customer import load_mock_customer
-from .retail import load_retail
--- mlpythonenv/lib/python3.6/site-packages/featuretools/synthesis/api.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-from .deep_feature_synthesis import DeepFeatureSynthesis
-from .dfs import dfs
-from .encode_features import encode_features
